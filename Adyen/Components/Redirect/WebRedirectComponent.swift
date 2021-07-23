8912440e139606131ac8cd2dbd599da27c6d7078
王胜峰-公司 <>
2021-07-23 15:38:18 +0800

    fix bug

--- Adyen/Components/Redirect/WebRedirectComponent.swift
+++ Adyen/Components/Redirect/WebRedirectComponent.swift
@@ -93,7 +93,14 @@ extension WebRedirectComponent: SFSafariViewControllerDelegate {
     /// Called when user clicks "Cancel" button.
     /// :nodoc:
     internal func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        delegate?.didFail(with: ComponentError.cancelled, from: self)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            if UIApplication.shared.applicationState == .active {
+                self.delegate?.didFail(with: ComponentError.cancelled, from: self)
+            }
+            else {
+                self.delegate?.didOpenExternalApplication(self)
+            }
+        }
     }
     
 }
